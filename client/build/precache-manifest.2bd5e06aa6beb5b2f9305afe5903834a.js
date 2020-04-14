d97297f97f0d30256b58472576167a8f74bf2354
Anne Homann <an.hmnn@gmail.com>
2020-04-14 14:06:14 +1000

    Final build of project for deployment

---
+++ client/build/precache-manifest.2bd5e06aa6beb5b2f9305afe5903834a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "206577efefdf941e17ee3e592fe99ce1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8b8d5382fadeb9ccb7d",
+    "url": "/static/css/2.051905ae.chunk.css"
+  },
+  {
+    "revision": "93bb682de9629978c097",
+    "url": "/static/css/main.3d40c4bd.chunk.css"
+  },
+  {
+    "revision": "a8b8d5382fadeb9ccb7d",
+    "url": "/static/js/2.2f0c2139.chunk.js"
+  },
+  {
+    "revision": "c1712df1def508d5080c79262ddc4224",
+    "url": "/static/js/2.2f0c2139.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93bb682de9629978c097",
+    "url": "/static/js/main.b5d9f8df.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  },
+  {
+    "revision": "8f4fbb62d381246e73056fe56da16f9f",
+    "url": "/static/media/Logo.8f4fbb62.png"
+  }
+]);
